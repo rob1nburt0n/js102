7cd459b8d4bab926dae89215800128b306e0096e
Robin Burton <1.robinburton@gmail.com>
2015-07-28 09:38:33 -0500

    links

---
+++ quiz102.js
@@ -0,0 +1,21 @@
+// 4. Write a JavaScript function that accepts an array as an argument, and returns a new array that is sorted.
+
+
+var names = ["carol", "robin", "anna", "gwen"];
+  var sortedNames = names.sort() {
+  return(sortedNames);
+  console.log(names);
+}
+
+
+
+// Write a JavaScript function that accepts an array, and returns a new array that contains elements in the array you passed in that are larger than 25.
+
+var numArray = [5, 10, 15, 20];
+
+var newNumArray = function(Addto) {
+ numArray = numArray.push(25, 30, 35, 40);
+ return(numArray);
+ console.log(numArray);
+
+}
