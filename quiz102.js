b27dee90be1d63582e7f85d0dc1553027677726c
Robin Burton <1.robinburton@gmail.com>
2015-07-29 18:13:47 -0500

    added for loops

--- quiz102.js
+++ quiz102.js
@@ -1,37 +1,49 @@
 // 4. Write a JavaScript function that accepts an array as an argument, and returns a new array that is sorted.
 
+$(document).ready(function() {
 
-var names = ["carol", "robin", "anna", "gwen"];
+  var inputArray = [];
+  var sortedArray = [];
+  var outputArray = [];
+  
 
 
-  function sort(names) {
-  	var sortedNames = names.sort();
-    return(sortedNames);
-    console.log(sortedNames);
+  function sorted(inputArray) {
+    sortedArray = inputArray.sort(function(a, b){return a-b}) 
+    return(sortedArray);
+    // console.log(sortedArray);
+
 }
-
-
-
 // Write a JavaScript function that accepts an array, and returns a new array that contains elements in the array you passed in that are larger than 25.
-
-var numArray = [5, 10, 15, 20];
-
-var newNumArray = function(Addto) {
- numArray = numArray.push(25, 30, 35, 40);
- return(numArray);
- console.log(numArray);
+ function largerThan25(sorted) {
+   console.log(sorted);
+  for (var i = 0; i < sorted.length; i++) {
+    if (sorted[i] > 25) {
+    outputArray.push(sorted[i]);
+   
+    }
+  }
 
 }
-
 // 7. Attach an event handler to the button click.
-
-var button = document.getElementById("click");
-  
-  button.onclick = buttonClickHandler; 
-
-  function buttonClickHandler() {
-  
-  	}
+$("#click").click(function() {
+  var input = $(".input").val();
+  var array = input.split(",");
+  sorted(array);
+  largerThan25(sortedArray);
+  var divString = "";
+  for (var i = 0; i < outputArray.length; i++) {
+     console.log(outputArray[i]);
+        
+        divString += '<div class="number">' + outputArray[i] + '</div>';
+        // console.log(divString);
+        
+     };    
+        $("#output").append(divString); 
+     
+  });
+    
+});
 
 
   // 8. When the button is clicked retrieve the value of the input box. You should enter in a comma-delimited list of integers (e.g. 10,3,5,67,22,etc..).
